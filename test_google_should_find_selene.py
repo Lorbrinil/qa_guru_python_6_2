7b8cf6dd3f5c578bd03c1728fd8c0111e3e4e1a8
DESKTOP-EG5TCKK\svetlana.popova <pss96.svetlana@gmail.com>
2023-06-06 21:15:02 +0300

    Добавление фикстуры и негативного теста для поиска

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,7 +1,18 @@
 from selene.support.shared import browser
 from selene import be, have
+import pytest
 
+@pytest.fixture
+def set_browser():
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+def test_positive_google_search(set_browser):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_negative_google_search(set_browser):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('1GKsPRYgZTT3BKNVdj7RuNiWCtQPXhq8UpVPkjJ26rUI').press_enter()
+    browser.element('[id="res"]').should(have.text('По запросу 1GKsPRYgZTT3BKNVdj7RuNiWCtQPXhq8UpVPkjJ26rUI ничего не найдено.'))
